e9b707e3331dc0061e35485114846b30f43be71d
tristn <tristn@users.noreply.github.com>
2022-06-02 11:58:47 -1000

    Fixed sidebar

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -4,19 +4,18 @@ import { Link as LinkR } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 
 export const SidebarContainer = styled.aside`
-    position: fixed;
-    z-index: 999;
-    width: 100%;
-    height: 100%
-    background: #0d0d0d;
-    display: grid;
-    align-items: center;
-    top: 0;
-    left: 0;
-    transition: 0.3s ease-in-out;
-    opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
-    top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
-    top: 0;
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  background: #0d0d0d;
+  display: grid;
+  align-items: center;
+  top: 0;
+  left: 0;
+  transition: 0.3s ease-in-out;
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
 `;
 export const CloseIcon = styled(FaTimes)`
   color: #fff;
@@ -67,7 +66,7 @@ export const SidebarLink = styled(LinkS)`
 `;
 
 export const SideBtnWrap = styled.div`
-  display: flex
+  display: flex;
   justify-content: center;
 `;
 
