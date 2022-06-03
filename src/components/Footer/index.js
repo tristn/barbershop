c24b444de421040ccda0db12f462bf4b485044b7
tristn <tristn@users.noreply.github.com>
2022-06-02 22:32:10 -1000

    Stylize Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink
+} from './FooterElements';
 
 const Footer = () => {
   return (
@@ -16,30 +25,28 @@ const Footer = () => {
                 <FooterLink to="/signin">Terms of Service</FooterLink>
               </FooterLinkItems>
               <FooterLinkItems>
-                <FooterLinkTitle>About Us </FooterLinkTitle>
-                <FooterLink to="/signin">How it works</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Investors</FooterLink>
-                <FooterLink to="/signin">Terms of Service</FooterLink>
+                <FooterLinkTitle>Contact Us </FooterLinkTitle>
+                <FooterLink to="/">Contact</FooterLink>
+                <FooterLink to="/">Support</FooterLink>
+                <FooterLink to="/">Destinations</FooterLink>
+                <FooterLink to="/">Sponsorships</FooterLink>
               </FooterLinkItems>
             </FooterLinksWrapper>
             <FooterLinksWrapper>
               <FooterLinkItems>
-                <FooterLinkTitle>About Us </FooterLinkTitle>
-                <FooterLink to="/signin">How it works</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Investors</FooterLink>
-                <FooterLink to="/signin">Terms of Service</FooterLink>
+                <FooterLinkTitle>Videos</FooterLinkTitle>
+                <FooterLink to="/">Submit Video</FooterLink>
+                <FooterLink to="/">Ambassadors</FooterLink>
+                <FooterLink to="/">Agency</FooterLink>
+                <FooterLink to="/">Influencer</FooterLink>
               </FooterLinkItems>
               <FooterLinkItems>
                 <FooterLinkTitle>About Us </FooterLinkTitle>
-                <FooterLink to="/signin">How it works</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Investors</FooterLink>
-                <FooterLink to="/signin">Terms of Service</FooterLink>
+                <FooterLink to="/">How it works</FooterLink>
+                <FooterLink to="/">Social Media</FooterLink>
+                <FooterLink to="/">Careers</FooterLink>
+                <FooterLink to="/">Investors</FooterLink>
+                <FooterLink to="/">Terms of Service</FooterLink>
               </FooterLinkItems>
             </FooterLinksWrapper>
           </FooterLinksContainer>
