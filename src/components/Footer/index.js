5053dd0f4d55056ebc0b2e6b6a1cd90d1c53b24b
tristn <tristn@users.noreply.github.com>
2022-06-02 23:25:15 -1000

    Styling Footer 2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import {
   FooterContainer,
   FooterWrap,
   FooterLinksContainer,
   FooterLinksWrapper,
   FooterLinkItems,
   FooterLinkTitle,
-  FooterLink
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink
 } from './FooterElements';
 
 const Footer = () => {
@@ -50,6 +63,35 @@ const Footer = () => {
               </FooterLinkItems>
             </FooterLinksWrapper>
           </FooterLinksContainer>
+          <SocialMedia>
+            <SocialMediaWrap>
+              <SocialLogo to="/">dolla</SocialLogo>
+              <WebsiteRights>
+                dolla © {new Date().getFullYear()} All rights reserved.
+              </WebsiteRights>
+              <SocialIcons>
+                <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                  <FaFacebook />
+                </SocialIconLink>
+                <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                  <FaInstagram />
+                </SocialIconLink>
+                <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                  <FaYoutube />
+                </SocialIconLink>
+                <SocialIconLink
+                  href="//www.twitter.com"
+                  target="_blank"
+                  aria-label="Twitter"
+                >
+                  <FaTwitter />
+                </SocialIconLink>
+                <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                  <FaLinkedin />
+                </SocialIconLink>
+              </SocialIcons>
+            </SocialMediaWrap>
+          </SocialMedia>
         </FooterWrap>
       </FooterContainer>
     </>
