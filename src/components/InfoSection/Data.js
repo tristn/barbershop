2b0a809aeafedcb9f3de40a278a276c44047743d
tristn <tristn@users.noreply.github.com>
2022-06-02 21:25:01 -1000

    Update Data.js

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -22,9 +22,9 @@ export const homeObjTwo = {
   lightText: false,
   lightTextDesc: false,
   topLine: 'Unlimited Access',
-  headLine: 'Unlimited Transactions with zero fees',
+  headLine: 'Login to your account at any time',
   description:
-    'Get access to our exclusive app that allows you to sen unlimited transactions without getting charged any fees.',
+    'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
   buttonLabel: 'Learn More',
   imgStart: true,
   img: require('../../images/svg-3.svg').default,
@@ -35,19 +35,19 @@ export const homeObjTwo = {
 };
 
 export const homeObjThree = {
-  id: 'about',
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: 'Premium Bank',
-  headLine: 'Unlimited Transactions with zero fees',
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join our Team',
+  headLine: 'Creating an account is extremely easy',
   description:
-    'Get access to our exclusive app that allows you to sen unlimited transactions without getting charged any fees.',
-  buttonLabel: 'Get started',
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: 'Start Now',
   imgStart: false,
-  img: require('../../images/svg-2.svg').default,
-  alt: 'Car',
-  dark: true,
-  primary: true,
-  darkText: false
+  img: require('../../images/svg-5.svg').default,
+  alt: 'Paper',
+  dark: false,
+  primary: false,
+  darkText: true
 };
