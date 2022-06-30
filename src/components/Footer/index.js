8839da3dda977ca0d857da9036d3bc36da0a231c
tristn <tristn@users.noreply.github.com>
2022-06-30 13:26:24 -1000

    index

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -35,45 +35,23 @@ const Footer = () => {
             <FooterLinksWrapper>
               <FooterLinkItems>
                 <FooterLinkTitle>About Us </FooterLinkTitle>
-                <FooterLink to="/signin">How it works</FooterLink>
                 <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Investors</FooterLink>
                 <FooterLink to="/signin">Terms of Service</FooterLink>
               </FooterLinkItems>
               <FooterLinkItems>
                 <FooterLinkTitle>Contact Us </FooterLinkTitle>
                 <FooterLink to="/">Contact</FooterLink>
                 <FooterLink to="/">Support</FooterLink>
-                <FooterLink to="/">Destinations</FooterLink>
-                <FooterLink to="/">Sponsorships</FooterLink>
-              </FooterLinkItems>
-            </FooterLinksWrapper>
-            <FooterLinksWrapper>
-              <FooterLinkItems>
-                <FooterLinkTitle>Videos</FooterLinkTitle>
-                <FooterLink to="/">Submit Video</FooterLink>
-                <FooterLink to="/">Ambassadors</FooterLink>
-                <FooterLink to="/">Agency</FooterLink>
-                <FooterLink to="/">Influencer</FooterLink>
-              </FooterLinkItems>
-              <FooterLinkItems>
-                <FooterLinkTitle>About Us </FooterLinkTitle>
-                <FooterLink to="/">How it works</FooterLink>
-                <FooterLink to="/">Social Media</FooterLink>
-                <FooterLink to="/">Careers</FooterLink>
-                <FooterLink to="/">Investors</FooterLink>
-                <FooterLink to="/">Terms of Service</FooterLink>
               </FooterLinkItems>
             </FooterLinksWrapper>
           </FooterLinksContainer>
           <SocialMedia>
             <SocialMediaWrap>
               <SocialLogo to="/" onClick={toggleHome}>
-                dolla
+                cutz
               </SocialLogo>
               <WebsiteRights>
-                dolla © {new Date().getFullYear()} All rights reserved.
+                cutz © {new Date().getFullYear()} All rights reserved.
               </WebsiteRights>
               <SocialIcons>
                 <SocialIconLink href="/" target="_blank" aria-label="Facebook">
